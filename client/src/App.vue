e4be77d98354b23d892ffb14b303a9d87993bfe4
Muhammad Atif akram <muhammadatif.te@gmail.com>
2020-11-11 01:33:34 +0500

    backend

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<div id='root'>
+   
+    <Header title="Jack's Tutor Reservation"></Header>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from './components/layouts/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+   
+}
+</script>
+
+<style>
+ 
+</style>
